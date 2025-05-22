4fb40ba2e06e521de9660ae1b5b7b9bfb38ae5b3
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-05-22 19:04:57 +0400

    Update

---
+++ Python/lesson/lesson 1.py
@@ -0,0 +1,42 @@
+Name = "Hareshan" #string = collection of letter/character
+Age = 18 #integers
+Marks = 98.3 #Decimal numbers with integer
+Pass = True or False #Boolean data
+
+# integer/float operations
+
+a = 5
+b = 6
+
+# Arithmetic operation
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+# Logical operation
+
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+print("Hello this is Gab")
+name = "Gab"
+print("Hello this is ",name)
+# F-string
+print(f"Hello my name is ",name)
+
+#string
+
+#index
+Name = "Hareshan"
+print(Name)
+print(Name[0])
+print(Name[1])
+print(Name.upper())
+print(Name.lower())
