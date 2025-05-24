e43752ab4327ff3335d190c6049a5f6c8dc1ef81
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-05-24 17:27:52 +0400

    Update

---
+++ Python/Project/project 1.py
@@ -0,0 +1,23 @@
+a = 13
+b = 20
+
+print(a+b)
+print(b-a)
+print(b*a)
+print(b/a)
+print(b%a)
+
+print(a==b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+
+print("My age is ",a)
+print(f"My age is ",a)
+Name = "Hareshan"
+print(Name)
+print(Name[4])
+print(Name[0])
+print(Name.upper())
+print(Name.lower())
