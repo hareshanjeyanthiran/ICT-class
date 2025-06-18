c44b65dfafd656fe9fa153cdf7f3b7504e2f6455
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-06-18 20:02:45 +0400

    Update

---
+++ Python/Project/Project 7.py
@@ -0,0 +1,17 @@
+import random
+
+secret_number = random.randint(1, 100)
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+while True:
+    guess = int(input("Take a guess: "))
+    
+    if guess < secret_number:
+        print("Too low. Try again!")
+    elif guess > secret_number:
+        print("Too high. Try again!")
+    else:
+        print("🎉 Congratulations! You guessed the correct number!")
+        break
