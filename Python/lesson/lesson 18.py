8188bfd37a1516003c74b4a49b8c62a74f23e7df
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-07-27 19:57:10 +0400

    Update

---
+++ Python/lesson/lesson 18.py
@@ -0,0 +1,10 @@
+def printnumber(n):
+    interaction = 0
+    print("The number entered by the user is:", n)
+    interaction += 1
+    print("Total interactions done by the code is ",interaction,"\n")
+
+
+printnumber(10)
+printnumber(20)
+print("\n with any 'n' the time taken by our code won't change")
