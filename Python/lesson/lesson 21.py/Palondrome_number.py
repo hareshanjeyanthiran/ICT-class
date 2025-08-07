2143eb7521c49627665491a98373539128c99197
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-08-07 17:56:24 +0400

    Update

---
+++ Python/lesson/lesson 21.py/Palondrome_number.py
@@ -0,0 +1,16 @@
+number = int(input("Enter your number: "))
+
+original_number = number
+reversed_number = 0
+
+while number > 0:
+    digit = number % 10
+    reversed_number = reversed_number * 10 + digit
+    number //= 10
+
+  
+if original_number == reversed_number:
+    print(f"{original_number} is palidrome")
+
+else:
+    print(f"{original_number} is not palidrome")
