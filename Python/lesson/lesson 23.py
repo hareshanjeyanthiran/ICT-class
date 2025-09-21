9c3689109c7e69e882abb70365ab32676a6d5572
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-09-21 19:49:08 +0400

    Update

---
+++ Python/lesson/lesson 23.py
@@ -0,0 +1,18 @@
+#Binery Operation
+num1 = 10
+num2 = 4
+
+#Using right shift operator on num 1
+print("\nnum1 >> 1 = ",num1 >> 1)
+
+#Using right shift operator num 2
+print("\nnum2 >> 1 = ",num2 >> 1)
+
+num1 = 10
+num2 = 4
+
+#Using left shift operator num1
+print("\nnum1 << 1 = ",num1 << 1)
+
+#Using left shift operator num2
+print("\nnum2 << 1 = ",num2 << 1)
