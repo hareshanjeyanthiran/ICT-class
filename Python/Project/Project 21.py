de6f721062bf9e85e963d265a9da7dccfd475b84
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-09-23 17:14:35 +0400

    Update

---
+++ Python/Project/Project 21.py
@@ -0,0 +1,17 @@
+
+num1 = 28
+num2 = 6
+
+
+print("\nnum1 >> 1 = ",num1 >> 1)
+
+
+print("\nnum2 >> 1 = ",num2 >> 1)
+
+num1 = 28
+num2 = 6
+
+print("\nnum1 << 1 = ",num1 << 1)
+
+
+print("\nnum2 << 1 = ",num2 << 1)
