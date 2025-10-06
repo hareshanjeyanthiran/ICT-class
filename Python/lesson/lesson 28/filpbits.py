e65e67534219b6c23025fa624ca571385ce4ca0d
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-10-06 18:19:38 +0400

    Update

--- Python/lesson/lesson 28/filpbits.py
+++ Python/lesson/lesson 28/filpbits.py
@@ -1,19 +1,23 @@
-#Program to find power set of a set
+# Program to find power set of a set
 
-import math;
+import math
 
-def printpowerset(set,SetSize):
-    #Find total elements possible in power set
-    powersetsize = (int)(math.pow(2,SetSize))
-    outer = 0,
-    inner = 0,
+def printpowerset(set, SetSize):
+    # Find total elements possible in power set
+    powersetsize = (int)(math.pow(2, SetSize))
+    outer = 0
+    inner = 0
 
-    for outer in range(0,powersetsize):
-        for inner in range(0,SetSize):
-            #Check if inner bit in outer is set if set then print inner element from set
-            
+    for outer in range(0, powersetsize):
+        for inner in range(0, SetSize):
+            # Check if inner bit in outer is set; if set, then print inner element from set
             if ((outer & (1 << inner)) > 0):
-                print(set[inner],end=" ")
-            print()
+                print(set[inner], end=" ")
+        print()
 
-size = 
+# Input set
+set = ['a', 'b', 'c']
+size = len(set)
+
+# Print power set
+printpowerset(set, size)
