8736c974f13c22fad43cf35f0b9c1a088671cb83
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-10-09 17:51:38 +0400

    Update

---
+++ SQL/lesson 1/main.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS students(
+student_id INT PRIMARY KEY ,
+name  VARCHAR(40),
+gender VARCHER(1),
+grade INT
+);
+
+INSERT INTO students (student_id,name , gender, grade)
+VALUES
+(1,"Hareshan","M",8),
+(2,"Rahul","M",6),
+(3,"sree","F",7)
+
+SELECT * FROM students;
