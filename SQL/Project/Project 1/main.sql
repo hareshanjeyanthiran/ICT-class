ba7a428caae146bbb222e9a85916db131677570d
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-10-13 17:18:50 +0400

    Update

---
+++ SQL/Project/Project 1/main.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Employes(
+emp_id INT PRIMARY KEY ,
+name  VARCHAR(40),
+gender VARCHAR(1),
+salary INT
+);
+
+INSERT INTO Employes (emp_id,name ,gender,salary) VALUES
+(1,"Rahul","M",80000),
+(2,"Kartik","M",60000),
+(3,"Etan","M",70000),
+(4,"Sree","F",90000),
+(5,"Jileshan","M",75000);
+
+SELECT * FROM Employes;
