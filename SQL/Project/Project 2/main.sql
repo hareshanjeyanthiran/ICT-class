51342dcf94f3c96335a21442b6f6c9ae7bc2bcd4
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-10-23 17:19:24 +0400

    Update

--- SQL/Project/Project 2/main.sql
+++ SQL/Project/Project 2/main.sql
@@ -10,7 +10,7 @@ INSERT INTO Employees (Employee_ID, First_Name, Last_Name, Department, Salary) V
 ('1', 'John', 'Doe', 'Sales', 55000.00),
 ('2', 'Jane', 'Smith', 'Marketing', 60000.00),
 ('3', 'Emily', 'Johnson', 'IT', 75000.00),
-('4', 'Michael', 'Brown', 'HR', 50000.00),
+('4', 'Michael', 'Brown', 'Manage', 50000.00),
 ('5', 'Jessica', 'Davis', 'Finance', 70000.00),
 ('6', 'Daniel', 'Wilson', 'IT', 80000.00);
 
